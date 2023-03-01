7da24619faea53d9ec9b217e34a81913ca3a7921
yamisora-sys <20520930@ms.uit.edu.vn>
2023-03-01 22:14:55 +0700

    add firebase

---
+++ src/config/Firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDae4wkagbKao45AUzPbERyAod-e3EL-fs",
+  authDomain: "web-seller-52ba6.firebaseapp.com",
+  projectId: "web-seller-52ba6",
+  storageBucket: "web-seller-52ba6.appspot.com",
+  messagingSenderId: "585940974557",
+  appId: "1:585940974557:web:f3068249f2993f75cf5434",
+  measurementId: "G-RL5Y85SBJR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
