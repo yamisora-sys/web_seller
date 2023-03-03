390df46ba2e9c967e8f22962e8dc6ac7d706d220
chonqk123 <91725926+chonqk123@users.noreply.github.com>
2023-03-03 17:31:36 +0700

    chon

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Route, Routes } from 'react-router-dom';
 import { Register } from '@components/Login/Register';
 import { Login } from '@components/Login/Login';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login/>}/>
+      <Route path="/" element={<Navbar/>}/>
     </Routes>
   );
 }
