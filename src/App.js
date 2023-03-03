63c7f5c1c705dc0e8778d328589ab7fa886c6002
chonqk123 <91725926+chonqk123@users.noreply.github.com>
2023-03-04 01:37:06 +0700

    1:37

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { Route, Routes } from 'react-router-dom';
 import { Register } from '@components/User/Register';
 import { Login } from '@components/User/Login';
 import { Logout } from '@components/User/Logout';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+
 function App() {
   return (
-    <Routes>
-      {/* test api */}
-      <Route path="/" element={<Logout/>}/>
-    </Routes>
+    <>
+    <Navbar/>
+    <Home/>
+    </>
   );
 }
-
 export default App;
