785a09b63c42502d09ab0f3cf9e34888d9df1507
chonqk123 <91725926+chonqk123@users.noreply.github.com>
2023-03-06 01:05:47 +0700

    1:05

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,12 @@ const Home = () => {
     return (
         <div>
             <div class="card text-bg-dark">
-                <img src={ImgBanner} class="card-img" alt="Background"
+                <img id="imgBanner" src={ImgBanner} class="card-img shadow-6" alt="Background"
                 height="550px"/>
-                    <div class="card-img-overlay">
+                    <div id ="textBanner"class="card-img-overlay d-flex flex-column justify-content-center">
                         <div className="container">
-                        <h2 class="card-title">COCO LEN</h2>
-                        <h6 class="card-text">Cửa hàng cung cấp các sản phẩm làm từ LEN.</h6>
+                        <h5 class="card-title display-3 fw-bolder mb-0">COCO LEN  </h5>
+                        <h6 class="card-text ">Cửa hàng cung cấp các sản phẩm làm từ LEN.</h6>
                         <h6 class="card-text"> Mang đến cho khách hàng những trải nghiệm thật thú vị khi đến với cửa hàng chúng tôi...</h6>
                         </div>
                     </div>
